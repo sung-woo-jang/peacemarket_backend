e949d7176e08106bf5f1d1ec348bc244543c1f71
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2022-08-02 14:52:14 +0900

    비밀번호 암호화

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,15 +2,19 @@ import { EntityRepository, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UserRole } from './entities/user.role.enum';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(createUserDto: CreateUserDto) {
     const { email, password, nickname, phoneNumber } = createUserDto;
 
+    const salt = await bcrypt.getSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = await this.create({
       email,
-      password,
+      password: hashedPassword,
       nickname,
       phoneNumber,
       role: UserRole.USER,
