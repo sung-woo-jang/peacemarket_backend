df31a53adbb7a0ffb88b89d4a8e2f8383a524637
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2022-08-02 12:40:02 +0900

    typeorm & dotenv setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('/api');
+  app.enableCors({ credentials: true, origin: true });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(5500);
 }
 bootstrap();
