73510c82677d1c303d9a746533a51aaa64629891
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2022-08-03 22:10:23 +0900

    Feature: 인증메일 전송 Base

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -8,8 +9,23 @@ export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private readonly userRepository: UserRepository,
+    private readonly mailerService: MailerService,
   ) {}
-  signUp(createUserDto: CreateUserDto) {
-    return this.userRepository.signUp(createUserDto);
+  async signUp(createUserDto: CreateUserDto) {
+    await this.sendMemberJoinEmail(createUserDto.email);
+    return await this.userRepository.signUp(createUserDto);
+  }
+
+  async sendMemberJoinEmail(email: string) {
+    const mailOptions = {
+      to: email, // list of receivers
+      subject: '평화마켓 회원가입 인증 메일입니다. ✔', // Subject line
+      html: ` 가입확인 버튼를 누르시면 가입 인증이 완료됩니다.<br/>
+        <form action=URL method="POST">
+          <button>가입확인</button>
+        </form>`, // HTML body content
+    };
+
+    return await this.mailerService.sendMail(mailOptions);
   }
 }
