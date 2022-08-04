71b013a2e2774b0b23d460e84429789c74449544
gyomdyung <seastory624@gmail.com>
2022-08-04 17:39:18 +0900

    :bug:Feature: 회원가입 이메일 인증 Demo

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PayloadDto } from 'src/users/dto/payload.dto';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async login(payload: PayloadDto) {
+    return jwt.sign({ ...payload }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPRIESIN,
+    });
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthService } from 'src/auth/auth.service';
 import { EmailService } from 'src/email/email.service';
@@ -12,6 +12,7 @@ export class UsersService {
     private emailService: EmailService,
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
+    private authService: AuthService,
   ) {}
 
   async signUp(createUserDto: CreateUserDto) {
@@ -31,8 +32,14 @@ export class UsersService {
   async verifyEmail(signupVerifyToken: string): Promise<string> {
     // TODO
     // 1. DB에서 signupVerifyToken으로 회원 가입 처리중인 유저가 있는지 조회하고 없다면 에러 처리
-    // 2. 바로 로그인 상태가 되도록 JWT를 발급
+    const user = await this.usersRepository.findOne({ signupVerifyToken });
+    if (!user) throw new NotFoundException('유저가 존재하지 않습니다.');
 
-    throw new Error('Method not implemented.');
+    return this.authService.login({
+      id: user.id,
+      email: user.email,
+      nickname: user.nickname,
+    });
+    // 2. 바로 로그인 상태가 되도록 JWT를 발급
   }
 }
