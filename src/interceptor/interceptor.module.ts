1fcd3f3313320782d6c3e40edf3b910dd90c74e2
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2022-08-05 22:52:55 +0900

    :sparkles:Feature: data transform 인터셉터 등록

--- src/interceptor/interceptor.module.ts
+++ src/interceptor/interceptor.module.ts
@@ -1,12 +1,13 @@
 import { Logger, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './logging.interceptor';
-import { TransformInterceptor } from './transfrom.interceptor';
+import { TransformInterceptor } from './transform.interceptor';
 
 @Module({
   providers: [
     Logger,
     { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
   ],
 })
 export class InterceptorModule {}
