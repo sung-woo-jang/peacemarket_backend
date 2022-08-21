c09ade44656944d74140397c751296e60d23b419
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2022-08-21 20:07:06 +0900

    :sparkles:Feature: Product Entity Base & 의존관계

--- src/domain/products/entities/product.entity.ts
+++ src/domain/products/entities/product.entity.ts
@@ -1,5 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsDate, IsNotEmpty } from 'class-validator';
+import { User } from 'src/domain/users/entities/user.entity';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ProductCategory } from './category.enum';
 
 @Entity()
 export class Product extends BaseEntity {
@@ -9,4 +19,36 @@ export class Product extends BaseEntity {
   })
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  // 제목
+  @Column()
+  title: string;
+
+  // 내용
+  @Column()
+  description: string;
+
+  // 가격
+  @Column({ type: 'int' })
+  price: number;
+
+  // 판매여부
+  @Column()
+  status: boolean;
+
+  // 등록시간
+  @CreateDateColumn()
+  @IsDate()
+  @IsNotEmpty()
+  createdAt: Date;
+
+  // 카테고리
+  @Column()
+  category: ProductCategory;
+
+  // 판매자
+  @ManyToOne(() => User, (user) => user.product)
+  user: User;
+
+  // 댓글
 }
