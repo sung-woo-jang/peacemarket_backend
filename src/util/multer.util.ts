42621da06834376f30474957cb4422d121b3d82d
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2022-08-23 11:24:21 +0900

    :package:Chore: multer 옵션

---
+++ src/util/multer.util.ts
@@ -0,0 +1,20 @@
+import { existsSync, mkdirSync } from 'fs';
+import { diskStorage } from 'multer';
+import * as path from 'path';
+
+export const registProductMulterOption = {
+  storage: diskStorage({
+    destination: function (req, file, cb) {
+      const uploadPath = path.join(__dirname, '../../', './image');
+      if (!existsSync(uploadPath)) {
+        // uploads 폴더가 존재하지 않을시, 생성.
+        mkdirSync(uploadPath);
+      }
+      cb(null, uploadPath);
+    },
+    filename: function (req, file, cb) {
+      file.originalname = Date.now() + '.' + file.mimetype.split('/')[1];
+      cb(null, file.originalname);
+    },
+  }),
+};
