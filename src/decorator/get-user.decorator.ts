04b2059e3cf29f7e4b2a87397af9de1b89761e56
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2022-08-28 15:01:30 +0900

    :recycle:Refactor: get-user 데코레이터 예외 추가

--- src/decorator/get-user.decorator.ts
+++ src/decorator/get-user.decorator.ts
@@ -1,9 +1,15 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { User } from 'src/domain/users/entities/user.entity';
 
 export const GetUser = createParamDecorator(
   (_, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
+    if (!req.user)
+      throw new UnauthorizedException('로그인 후 이용할 수 있습니다.');
     return req.user;
   },
 );
